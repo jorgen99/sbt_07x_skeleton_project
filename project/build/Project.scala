e30b5535a9a662962b3b8e7f8a6d73b157214c21
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2011-06-08 13:26:36 +0200

    Removed build from .gitignore and added sbt setup

---
+++ project/build/Project.scala
@@ -0,0 +1,18 @@
+import sbt._
+import templemore.sbt.CucumberProject
+
+class TheProj(info: ProjectInfo) extends DefaultProject(info) with CucumberProject {
+
+  val scalaToolsSnapshots = ScalaToolsSnapshots
+
+  val scalatest = "org.scalatest" %% "scalatest" % "1.4.1" % "test"
+  val specs2 = "org.specs2" %% "specs2" % "1.3"
+  val scalaz = "org.specs2" %% "scalaz-core" % "6.0.RC2"
+
+  def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
+
+  override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
+
+  val snapshots = "snapshots" at "http://scala-tools.org/repo-snapshots"
+  val releases = "releases" at "http://scala-tools.org/repo-releases"
+}
